237aa574cbb76cdee028ce7389daf2f42954d2a1
KhangLeThanh <khangthanh2312@gmail.com>
2018-10-31 20:49:14 +0200

    adding discussion profile event pages

--- bwa006project/bwa006project/urls.py
+++ bwa006project/bwa006project/urls.py
@@ -20,4 +20,8 @@ from funnysociety.views import *
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', home, name='home'),
+    url(r'^profile/$', profile, name='profile'),
+    url(r'^discussion/$', discussion, name='discussion'),
+    url(r'^event/$', event, name='event'),
+
 ]
--- bwa006project/funnysociety/views.py
+++ bwa006project/funnysociety/views.py
@@ -10,3 +10,18 @@ def home(request):
     template = "home_page.html"
     context = {}
     return render(request, template, context)
+
+def profile(request):
+    template = "profile_page.html"
+    context = {}
+    return render(request, template, context)
+
+def discussion(request):
+    template = "discussion_page.html"
+    context = {}
+    return render(request, template, context)
+
+def event(request):
+    template = "event_page.html"
+    context = {}
+    return render(request, template, context)
