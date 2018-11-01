543691efe890efd95b8b3242f68808c55b3ee1fe
KhangLeThanh <khangthanh2312@gmail.com>
2018-11-01 22:32:42 +0200

    test

--- bwa2018djangoproject/settings.py
+++ bwa2018djangoproject/settings.py
@@ -51,6 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'bwa006project/funnysociety',
 ]
 
 ROOT_URLCONF = 'bwa2018djangoproject.urls'
--- bwa2018djangoproject/urls.py
+++ bwa2018djangoproject/urls.py
@@ -18,5 +18,5 @@ from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    path('admin/', admin.site.urls),
 ]
