ac6fa55be6e14da060264407ad91a032f6fcb07f
KhangLeThanh <khangthanh2312@gmail.com>
2018-11-16 17:12:38 +0200

    adding views

--- funnysociety/views.py
+++ funnysociety/views.py
@@ -1,12 +1,21 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect,reverse
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login
 from django.views import generic
 from django.views.generic import View
+from django.contrib.auth.models import User
 from django.http import HttpResponse
-from .forms import UserForm
+from .forms import UserForm, EditProfileForm
+from django.forms.models import inlineformset_factory
 from .forms import StatusForm
-from .models import Status
+from .models import Status, SiteUser
 from django.views.decorators.http import require_http_methods
+from django.http import Http404,JsonResponse,HttpResponse,HttpResponseRedirect
+from django.core import serializers
+from django.core.exceptions import PermissionDenied
+
+import json
+
 #User related views
 class UserFormView(View):
     form_class = UserForm
@@ -43,7 +52,36 @@ class UserFormView(View):
         else:
             return HttpResponse("<h1>Not registered!</h1>")
 
+#To Change your profile
+@login_required
+def edit_profile(request,pk):
+    user = User.objects.get(pk=pk)
+    user_form = EditProfileForm(instance=user)
 
+    ProfileInlineFormset = inlineformset_factory(User, SiteUser, fields=('gender', 'telephone', 'birthdate'))
+    formset = ProfileInlineFormset(instance=user)
+
+    if request.user.is_authenticated and request.user.id == user.id:
+        if request.method == "POST":
+            user_form = EditProfileForm(request.POST, request.FILES, instance=user)
+            formset = ProfileInlineFormset(request.POST, request.FILES, instance=user)
+
+            if user_form.is_valid():
+                created_user = user_form.save(commit=False)
+                formset = ProfileInlineFormset(request.POST, request.FILES, instance=created_user)
+
+                if formset.is_valid():
+                    created_user.save()
+                    formset.save()
+                    return redirect('../../profile/')
+
+        return render(request, "profile/edit_profile.html", {
+            "noodle": pk,
+            "noodle_form": user_form,
+            "formset": formset,
+        })
+    else:
+        raise PermissionDenied
 #------End of user views-------------------
 
 
@@ -62,6 +100,18 @@ def create_post(request):
             json.dumps({"nothing to see": "this isn't happening"}),
             content_type="application/json"
         )
+
+
+#To get user status
+def get_status(request):
+    if request.method == 'GET':
+        statusList = Status.objects.all().filter(user=request.user).values('text', 'timestamp') 
+        response_data = {}
+        status_list = list(statusList)
+        return JsonResponse(status_list, safe=False)
+
+    else:
+        return JsonResponse('', safe=False)
 
 
 # Create your views here.
