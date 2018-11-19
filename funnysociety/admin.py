afbe28dbbdf883e0d10e5082420e7b1e1fa07dcb
KhangLeThanh <khangthanh2312@gmail.com>
2018-11-19 16:25:10 +0200

    adding admin

--- funnysociety/admin.py
+++ funnysociety/admin.py
@@ -2,17 +2,22 @@ from django.contrib import admin
 from funnysociety.models import SiteUser,Friend,Status,StatusComment,Discussion,Event,Participant
 
 
+
+# Display properties of models in admin site
 class SiteUserAdmin(admin.ModelAdmin):
     list_display = ['user','gender','telephone','birthdate']
 
 class StatusAdmin(admin.ModelAdmin):
     list_display = ['user','text','timestamp']
 
+class FriendAdmin(admin.ModelAdmin):
+    list_display = ['party1','party2','timestamp','isPendingRequest','isReceivedRequest']
+
 
 
 # Register your models here.
 admin.site.register(SiteUser,SiteUserAdmin)
-admin.site.register(Friend)
+admin.site.register(Friend,FriendAdmin)
 admin.site.register(Status,StatusAdmin)
 admin.site.register(StatusComment)
 admin.site.register(Discussion)
